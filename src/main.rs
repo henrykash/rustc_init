be85bb8a67d5a65481d1b491b727a47195e1afb8
henrykash <henrykariuki003@gmail.com>
2023-07-03 15:25:00 +0300

    learnt about varibale & how to assign varibales

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+
+    //variables declaration in rust
+    let _num1: i32 =9;
+    let _num2: f32 =9.2;
+
+    //use the word mut if you want to reassign the variable
+
+    let mut _num3: i32 = 3;
+
+    _num3 = 8;
+
+    print!( "{} {} {}",_num1, _num2, _num3);
 }
+
