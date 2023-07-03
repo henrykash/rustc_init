636458528fdd807f3853d1ca779ea2e377027d03
henrykash <henrykariuki003@gmail.com>
2023-07-03 16:17:32 +0300

    learning about strings

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
 
-    //variables declaration in rust
+    /*variables declaration in rust */
     let _num1: i32 =9;
     let _num2: f32 =9.2;
 
@@ -10,6 +10,24 @@ fn main() {
 
     _num3 = 8;
 
-    print!( "{} {} {}",_num1, _num2, _num3);
-}
+    print!( "{} {} {} ",_num1, _num2, _num3);
 
+
+    /*immutable literal stored in read-pn;y data */
+
+    let name1: &'static str = "\nEgineer kash"; //this string can not be modified only read
+
+    println!("{} ", name1);
+
+    /*create mutable growable string on heap */
+    let mut name2: String = String::new();
+    name2 = name2+ "enginer";
+    name2.push_str(" man");
+
+    println!("{} ", name2);
+
+    /* string slices: a mutable portion of the string */
+
+
+
+}
