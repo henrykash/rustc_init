ae9dde78097e536af2e657698eeb9006227ec675
henrykash <henrykariuki003@gmail.com>
2023-07-04 23:27:16 +0300

    Learning about functions and modules

--- part-2/src/main.rs
+++ part-2/src/main.rs
@@ -1,16 +1,20 @@
 fn main() {
     println!("Hello, world!");
    printing::goodbye();
-   printing::time_stuff::give_us_the_time();
-   printing::time_stuff::give_us_the_time2();
+   printing::announce_time();
+   printing::give_us_the_time2();
 }
 
 
-//provides a way for us to organize our code into groups of related functionality
+//modules:: provides a way for us to organize our code into groups of related functionality
 mod printing{
 
    //nestting modules help in organizing related functionality
    pub mod time_stuff;
+   pub use time_stuff::give_us_the_time as announce_time; //renaming functions when importing them
+    pub use time_stuff::give_us_the_time2;
+
+
    pub fn goodbye() {
         println!("Goodbye, world!");
     }
