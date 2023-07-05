278ab7fcc36e360e41e60b9d9c74dc59564c975d
henrykash <henrykariuki003@gmail.com>
2023-07-05 14:18:30 +0300

    Learning about Control flow in rust

--- part5/src/main.rs
+++ part5/src/main.rs
@@ -24,5 +24,22 @@ fn fin(n: i32) -> i32 {
         }
         return b;
     }
+
+//     //or we can use loop statement
+//     loop {
+//         let next = a + b; // next number in the series
+//         a = b;
+//         b = next;
+//         x += 1;
+
+//         if x < n {
+//             break;
+//         }
+//     break
+//     }
+//     return b;
+// }
+
+
 }
 
