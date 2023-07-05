318bbe76e68560f519130ca3e5c1c0c5f6ec1cec
henrykash <henrykariuki003@gmail.com>
2023-07-05 14:13:46 +0300

    Learning about Control flow in rust

---
+++ part5/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    println!("Learning control flow in Rust using finabonacci series");
+       let n = 42;
+    print!("Finabonacci F{} = {} ", n, fin(n));
+}
+
+//conditional statements in rust
+fn fin(n: i32) -> i32 {
+    if n == 0 {
+        return 0;
+    } else if n == 1 {
+        return 1;
+    } else {
+        let mut x = 1;
+        let mut a = 0;
+        let mut b = 1;
+       
+       //using while loop
+        while x < n {
+            let next = a + b; // next number in the series
+            a = b;
+            b = next;
+            x = x + 1;
+        }
+        return b;
+    }
+}
+
