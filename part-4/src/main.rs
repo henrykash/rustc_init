1f9486e6fd5cbb63fe113b0797526f534860e425
henrykash <henrykariuki003@gmail.com>
2023-07-05 13:21:28 +0300

    Learning More about functions and Test-Driven Development in Rust

--- part-4/src/main.rs
+++ part-4/src/main.rs
@@ -5,7 +5,17 @@ fn main() {
     println!("The answer is: {}", answer);
 }
 
+//functions are declared with fn keyword, and the return type is specified after an arrow ->
 fn find_answer(x: i32, y: i32) -> i32 {
-    let answer = x + y + 10;
+    let answer = x * y + 10;
     answer  
+}
+
+//writing tests in Rust is very easy, just add #[test] attribute to any function and run cargo test
+#[test]
+fn test_find_answer() {
+    let x = 5;
+    let y = 6;
+    let answer = find_answer(x, y);
+    assert_eq!(40, answer);
 }
