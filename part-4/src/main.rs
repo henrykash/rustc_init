f63426ef6938abd02a34763d2a65c5d32ec917a1
henrykash <henrykariuki003@gmail.com>
2023-07-05 13:55:33 +0300

    Learning about Functions and Test_Driven Development

--- part-4/src/main.rs
+++ part-4/src/main.rs
@@ -4,6 +4,22 @@ fn main() {
    let answer = find_answer(x, y);
     println!("The answer is: {}", answer);
 }
+#[cfg(test)]
+mod tests {
+    #[allow(unused_imports)]
+    use super::find_answer; //import the function from the parent module or use super::* to import all items from the parent module i.e use super::*
+
+ //tests are declared with #[test] attribute
+#[test]
+pub fn test_find_answer() {
+    let x = 2;
+    let y = 2;
+    let answer = find_answer(x, y);
+    assert_eq!(14, answer);
+}
+
+}
+
 
 //functions are declared with fn keyword, and the return type is specified after an arrow ->
 fn find_answer(x: i32, y: i32) -> i32 {
@@ -11,11 +27,5 @@ fn find_answer(x: i32, y: i32) -> i32 {
     answer  
 }
 
-//writing tests in Rust is very easy, just add #[test] attribute to any function and run cargo test
-#[test]
-fn test_find_answer() {
-    let x = 5;
-    let y = 6;
-    let answer = find_answer(x, y);
-    assert_eq!(40, answer);
-}
+
+
