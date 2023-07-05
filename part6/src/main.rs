0842cdd62668e391c31ab3c525b28e82d81d3611
henrykash <henrykariuki003@gmail.com>
2023-07-05 19:54:59 +0300

    Learning about Methods, Ownership and Borrowing

--- part6/src/main.rs
+++ part6/src/main.rs
@@ -1,39 +1,73 @@
-struct  Item {
-    what: String,
+use model::{Hands, Item};
+
+mod model {
+pub struct  Item {
+    what: String, //string owned by the item
     present: bool,
 }
 
-struct Hands {
+pub struct Hands {
     left: Item,
     right: Item ,
 }
 
-fn main() {
+impl Hands {
 
-    let hands: Hands = Hands {
-        left: Item {
-            what: String::from("banana"),
-            present: true,
-        },
-        right: Item {
-            what: String::from("apple"),
-            present: true,
-        },
-    };
-
-    if hands.right.present {
-        println!("Right hand is holding {} ", hands.right.what);
-    } else {
-        println!("Right hand is empty");
+  pub  fn new()-> Hands {
+        Hands {
+            left: Item {
+                what: String::from("banana"),
+                present: true,
+            },
+            right: Item {
+                what: String::from("apple"),
+                present: true,
+            },
+        }
+    
     }
+    
 
-
-    if hands.left.present {
-        println!("Left hand is holding {} ", hands.left.what);
-    } else {
-        println!("Left hand is empty");
-            
+   pub fn juggle( mut hands: Hands)-> Hands{
+        print!("Juggling ...");
+        let air  = hands.left;
+        hands.left = hands.right;
+        hands.right = air;
+    
+        return hands;
     }
+   pub fn report(hands: &Hands) {
+        Item::report_item(&hands.left, "Left");
+        Item::report_item(&hands.right, "Right");
+    }   
+    
+}
+
+impl Item {
+    fn report_item(item: &Item, which: &str) {
+        if item.present {
+            println!("{} hand is holding {} ", which ,item.what);
+        } else {
+            println!("{} hand is empty", which);
+        }
+    }
+    
 
 }
+
+}
+
+fn main() {
+    #[allow(unused_mut)]
+    let mut hands: Hands = Hands::new(); // call new() with no arguments
+    Hands::report(&hands); // call report() with a reference to hands
+
+   hands = Hands::juggle(hands); // call juggle() with a mutable reference to hands
+
+    Hands::report(&hands); // call report() with a reference to hands
+
+}
+
+
+
 
