1d44b41ea33bff01db380ffec4bc7a88f565db62
henrykash <henrykariuki003@gmail.com>
2023-07-06 13:49:42 +0300

    Learning about Generics & Traits

--- part7/src/main.rs
+++ part7/src/main.rs
@@ -1,26 +1,52 @@
 use model::Hands;
 
 mod model {
-    pub enum Item {
-         Something(String), //string owned by the item
-         Nothing
+
+    //trait is a collection of methods that can be implemented by any type
+    pub trait Displayable {
+        fn display(&self) -> String;
     }
 
+    #[allow(dead_code)]
+    enum Fruit {
+        Apple,
+        Banana,
+        Orange,
+    }
+
+    impl Displayable for Fruit {
+        // display() is a method of the Fruit enum type (self is the enum instance)
+        fn display(&self) -> String {
+            match self {
+                Fruit::Apple => String::from("Apple"),
+                Fruit::Banana => String::from("Banana"),
+                _ => String::from("Orange"), // _ is a catch-all pattern (like default in a switch)
+            }
+        }
+    }
+
+    #[allow(dead_code)]
+    // Item is a generic enum type (T is a type parameter): a pattern that can be instantiated with different types
+    // pub enum Item<T> {
+    //     Something(T), //string owned by the item
+    //     Nothing,
+    // }
+
     pub struct Hands {
-        left: Item,
-        right: Item,
+        left: Option<Fruit>, //instanciate the Item enum with the Fruit enum
+        right: Option<Fruit>,
     }
 
     impl Hands {
         pub fn new() -> Self {
             Hands {
-                left: Item::Something(String::from("apple")),
-                right: Item::Something(String::from("banana"))
+                left: Option::Some(Fruit::Apple),
+                right: Option::Some(Fruit::Banana),
             }
         }
 
         pub fn juggle(mut self) -> Hands {
-            print!("Juggling ...");
+            print!("\nJuggling ...");
             let air = self.left;
             self.left = self.right;
             self.right = air;
@@ -28,19 +54,22 @@ mod model {
             return self;
         }
         pub fn report(&self) {
-            Item::report_item(&self.left, "Left");
-            Item::report_item(&self.right, "Right");
+             report_item(&self.left, "Left");
+             report_item(&self.right, "Right");
         }
     }
-
-    impl Item {
-        fn report_item(&self, which: &str) {
-            if let Item::Something(what) = self {
-                println!("\n{} hand is holding {} ", which, what);
-            } else {
-                println!("\n{} hand is empty", which);
+    // T is a type parameter (generic type) that implements the Displayable trait (T: Displayable)
+    
+        fn report_item<T: Displayable> (item: &Option<T>, which: &str) {
+            match item {
+                Option::Some(what) => {
+                    println!("\n{} hand is holding {} ", which, what.display());
+                }
+                _ => {
+                    println!("\n{} hand is empty", which);
+                }
             }
-        }
+        
     }
 }
 
@@ -52,5 +81,4 @@ fn main() {
     hands = Hands::juggle(hands); // call juggle() with a mutable reference to hands
 
     Hands::report(&hands); // call report() with a reference to hands
-
 }
