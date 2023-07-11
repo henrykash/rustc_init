6e1e2afbd5d36fa5147973fe18a3d6b9f7b88a15
henrykash <henrykariuki003@gmail.com>
2023-07-11 14:42:21 +0300

    Learning about Rust Collections

--- part8/src/main.rs
+++ part8/src/main.rs
@@ -1,6 +1,7 @@
 use std::collections::{HashMap, VecDeque, BTreeMap, HashSet, BinaryHeap};
 
 fn main() {
+
     // VECTORS : vector provides dynamic array of a given type: List
     #[allow(unused_mut)]
     let mut primes = vec![2, 3, 5, 7, 11, 13, 17, 19];
@@ -8,16 +9,15 @@ fn main() {
     //iterating through a a vector using a while let loop
     let mut primes_iter = primes.iter().enumerate();
     while let Some((i, p)) = primes_iter.next() {
-        println!("Into the while let\n {}: {}", i, p);
+        println!("Vectors: while loop {}: {}", i, p);
     }
 
     //iterating through a vector using a for loop: which is designed to work with iterators exclusively
     for (i, p) in primes.iter().enumerate() {
-        println!("Into the for {}: {}", i, p);
+        println!(" Vectors: for loop {}: {}", i, p);
     }
 
     //VECDEQUE: used when we want to add or remove things in the front or end of a list i.e Vec
-
     let mut primes_n = VecDeque::new();
     primes_n.push_back(2);
     primes_n.push_back(3);
