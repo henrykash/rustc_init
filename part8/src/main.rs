8a73c1e2f1ce0778cd865974ae3b40de243d7975
henrykash <henrykariuki003@gmail.com>
2023-07-11 13:16:56 +0300

    Learning about Rust Collections

--- part8/src/main.rs
+++ part8/src/main.rs
@@ -1,12 +1,40 @@
+use std::collections::VecDeque;
+
+
 fn main() {
-    println!("Hello, world!");
+
+    // VECTORS : vector provides dynamic array of a given type: List
+    #[allow(unused_mut)]
     let mut primes = vec![2, 3, 5, 7, 11, 13, 17, 19];
-    primes.push(23);
-    println!("{:?}", primes);
 
-    //remove the last element
-    primes.remove(primes.len() - 1);
+ 
+  //iterating through a a vector using a while let loop
+    let mut primes_iter = primes.iter().enumerate();
+  while let Some((i, p)) = primes_iter.next() {
+    println!("Into the while let\n {}: {}", i, p);
 
-    //print the vector
-    println!("{:?}", primes);
-}
+  } 
+
+  //iterating through a vector using a for loop: which is designed to work with iterators exclusively
+    for (i, p) in primes.iter().enumerate() {
+        println!("Into the for {}: {}", i, p);
+    }
+
+  //VECDEQUE: used when we want to add or remove things in the front or end of a list i.e Vec
+    
+    let mut primes_n = VecDeque::new();
+    primes_n.push_back(2);
+    primes_n.push_back(3);
+    primes_n.push_front(5);
+      
+    //iterating through a a VecDeque using a while let loop
+    for (i, p) in primes_n.iter().enumerate() {
+        println!("Into the VeqDeque {}: {}", i, p);
+    }
+
+
+    //HASHMAP: used to store key-value pairs
+
+    
+
+ }
