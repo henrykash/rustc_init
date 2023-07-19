a76092bec0fcaaa3db4dcb4c83c201f2d35d82f2
henrykash <henrykariuki003@gmail.com>
2023-07-19 14:23:45 +0300

    Lifetimes

--- part9/src/main.rs
+++ part9/src/main.rs
@@ -29,19 +29,31 @@ impl<'a> Iterator for OurIterator<'a> {
     }
 }
 
+impl<'a> Drop for OurIterator<'a> {
+    fn drop(&mut self) {
+        println!("Dropping OurIterator");
+    }
+}
 fn main() {
     let mut nums = vec![1, 2, 3];
     let mut nums2 = OurIterator::new(&nums, 2);
     let mut nums3 = OurIterator::new(&nums, 3);
 
+    
+
     while let Some(num) = nums2.next() {
         println!("{}", num);
     }
+
+    // nums2 is dropped here
+    drop(nums2);
 
     while let Some(num) = nums3.next() {
         println!("{}", num);
     }
 
+    drop(nums3);
+    
    let _push = nums.push(4);
 
     print!("nums: {:?}", nums);
